a8eddda20d70323b221c4907891e1772894ac7fb
Felipe Ribeiro <felipeespindola.ti@gmail.com>
2025-01-19 11:56:53 -0300

    feat: initial commit

--- ecs.tf
+++ ecs.tf
@@ -2,18 +2,19 @@ resource "aws_ecs_cluster" "main" {
   name = var.project_name
 
   setting {
-    name = "container-insights"
+    name  = "containerInsights"
     value = "enabled"
   }
 }
 
-
 resource "aws_ecs_cluster_capacity_providers" "main" {
   cluster_name = aws_ecs_cluster.main.name
+
   capacity_providers = var.capacity_providers
+
   default_capacity_provider_strategy {
-     base = 1
-     weight = 100
-     capacity_provider = "FARGATE"
+    base = 1
+    weight = 100
+    capacity_provider = "FARGATE"
   }
 }
